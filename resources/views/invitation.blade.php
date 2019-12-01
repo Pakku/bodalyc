a1a5a438070d8442557917ce8510fdc2e8287346
Pakku <quentyaiden@gmail.com>
2019-12-02 00:47:11 +0100

    Design of invitation.

--- resources/views/invitation.blade.php
+++ resources/views/invitation.blade.php
@@ -1,18 +1,35 @@
 @extends('layouts.invitation')
 
 @section('content')
+<div class="opacity-layer">
+</div>
 <div class="container">
-    <div class="row big-name justify-content-center">
-        <div class="col-md-8">
-            <div class="row names-wrapper">
-                <div class="col-4 ampersand">
+    <div class="quote">
+        "Esta parte de mi vida, este pequeño momento, lo llamo felicidad"
+    </div>
+    <div class="introduction">
+        <div class="first-part">Aurita y Pedro</div>
+        <div class="second-part">Te invitamos a nuestra boda</div>
+    </div>
+    <div class="row big-name">
+        <div class="col-md-12">
+            <div class="names-wrapper">
+                <div class="ampersand">
                     &
                 </div>
-                <div class="col-8 names">
+                <div class="names">
                     Lucía <br />
                     Carlos
                 </div>
             </div>
+        </div>
+    </div>
+    <div class="after-wrapper">
+        <div class="date">
+            29 de Febrero, 2020
+        </div>
+        <div class="more-information">
+            <button class="button js-register">Más información</button>
         </div>
     </div>
 </div>
