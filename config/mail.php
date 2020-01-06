c48be4367628f8307843f306cf23d873408bc5c6
Pakku <quentyaiden@gmail.com>
2020-01-06 10:39:04 +0100

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'info@luciaycarlos.com'),
+        'name' => env('MAIL_FROM_NAME', 'Lucía y Carlos'),
     ],
 
     /*
